1a01cfb922aedb401d8a7ec17884b70be5054204
Lee-sugyeong <653waterscape@gmail.com>
2026-08-16 23:34:07 +0900

    최대최소

---
+++ C/최소공배수.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+    int n, a, b;
+
+    scanf("%d", &n);
+    int arr;
+
+    for(int i = 0 ; i < n; i++){
+        scanf("%d %d",&a, &b);
+        arr[i] = a * b;
+    }
+    for(int j = 0; j < n; j++){
+        printf("%d\n", arr[j]);
+    }
+    return 0;
+}
